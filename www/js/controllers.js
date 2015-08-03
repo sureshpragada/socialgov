2deb5e1b8430870cc793db8d8d39ca07718ab627
Suresh Pragada <spragada@lendingclub.com>
2015-08-02 17:57:10 -0700

    Parse intergration with activity page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,66 @@
 angular.module('starter.controllers', [])
 
 .controller('ActivityCtrl', function($scope, $http) {
-  $http.get("js/activity.json").success(function(data) {
-    $scope.activities=data;
+  // $http.get("js/activity.json").success(function(data) {
+  //   $scope.activities=data;
+  // });
+
+  //$scope.activities=[];
+
+  var Activity=Parse.Object.extend("Activity");
+  var query=new Parse.Query(Activity);
+  query.equalTo("regionUniqueName", "dowlaiswaram");
+  query.find({
+    success: function(results) {
+    $scope.activities=results;
+
+      // for(var i=0;i<results.length;i++) {
+      //   var object=results[i];
+      //   var activity={"activityType": object.get("activityType"), 
+      //     "notifyMessage": object.get("notifyMessage"), 
+      //     "regionUniqueName": object.get("regionUniqueName"),
+      //     "createdAt": object.createdAt};
+      //   $scope.activities.push(activity);
+      // }
+
+
+    }, 
+    error: function(error) {
+      alert("Unable to get activities");
+    }
   });
 })
 
 .controller('PostActivityCtrl', function($scope, $http, $location) {
   
-  $scope.post={"firstName":"", "lastName":"", "message": "", "type":"announcement", "region":"Dowlaiswaram"};
+  $scope.post={"activityType": "NOTF", "notifyMessage": "", "regionUniqueName":"dowlaiswaram"};
 
   $scope.submitPost=function() {
-    var post=$scope.post;
-    alert(post.firstName + post.lastName + post.message + post.type + post.region);
+      //var post=$scope.post;
+      // alert(post.activityType + post.message + post.region);
+
+      var Activity = Parse.Object.extend("Activity");
+      var activity = new Activity();
+
+      // activity.set("activityType", post.activityType)    
+      // activity.set("notifyMessage", post.message);
+      // activity.set("regionUniqueName", post.region)
+
+      activity.save($scope.post, {
+      success: function(activity) {
+        // Push the new notification to the top of activity chart, probably through Activity service
+        // alert("Activity ID : " + activity.id);
+      },
+      error: function(activity, error) {
+        // Notify user that post has failed
+        $scope.postError=true;
+        $scope.postErrorMessage=error;
+      }
+    });
+    $location.path("/tab/dash");
+  };
+
+  $scope.cancelPost=function(){
     $location.path("/tab/dash");
   };
 
