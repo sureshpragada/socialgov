f46dc85e32dd75841baf8daf875b09fe96f7486b
pradeepKaatnam <pradeep.kaatnam@gmail.com>
2015-09-16 23:05:16 +0530

    Added AdminAccessReqCtrl, AdminAccessReqDetailCtrl, and made some changes to AccountCtrl
    To create and save the admin access requests.
    To handle the admin access requests.
    To update the user object.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,6 +9,7 @@ angular.module('starter.controllers', ['ngCordova', 'ionic'])
   var Activity=Parse.Object.extend("Activity");
   var query=new Parse.Query(Activity);
   query.equalTo("regionUniqueName", user.get("residency"));
+  //console.log(user.id);
   query.include("user");
   query.descending("createdAt");
   query.find({
@@ -88,10 +89,118 @@ angular.module('starter.controllers', ['ngCordova', 'ionic'])
   });
 })
 
+.controller('AdminAccessReqCtrl', function($scope, $state) {
+  $scope.adminDetails={};
+  $scope.adminRole=true;
+  $scope.adminRequestErrorMessage=null;
+  console.log($scope.adminDetails.role);
+  $scope.isSelected=function() {
+      $scope.adminRole=false;
+  };
+  $scope.submitDetails=function() {    
+      if($scope.adminDetails.role!=null && $scope.adminDetails.title!=null && $scope.adminDetails.contributeMessage!=null &&$scope.adminDetails.contributeMessage.length>10 && $scope.adminDetails.contributeMessage.length<1024){
+        var AccessRequest = Parse.Object.extend("AccessRequest");
+        var accessRequest = new AccessRequest();
+        accessRequest.set("role",$scope.adminDetails.role);
+        accessRequest.set("title",$scope.adminDetails.title);
+        accessRequest.set("contributeMessage",$scope.adminDetails.contributeMessage);
+        accessRequest.set("status","PEND");
+        //accessRequest.set("userId",Parse.User.current().id);
+        accessRequest.set("user",Parse.User.current());
+        console.log(JSON.stringify(accessRequest));
+        accessRequest.save(null, {
+          success: function(accessRequest) {
+            //alert('New object created with objectId: ' + accessRequest.id);
+            $scope.$apply(function(){
+              $state.go("tab.account");    
+            });
+          },
+          error: function(accessRequest, error) {
+            // Execute any logic that should take place if the save fails.
+            // error is a Parse.Error with an error code and message.
+            //alert('Failed to create new object, with error code: ' + error.message);
+            console.log("Error in posting message " + error.message);
+            $scope.adminRequestErrorMessage=error.message;
+          }
+        });
+      }else{
+          $scope.adminRequestErrorMessage="Fill the details properly";
+      }
+  };
+  $scope.cancel=function(){
+    $state.go("tab.account");
+  };
+})
+
 .controller('RegionDetailCtrl', function($scope, $stateParams, RegionService) {
   RegionService.all(function(data) {
     $scope.region=RegionService.get(data, $stateParams.regionUniqueName);
   });
+})
+
+.controller('AdminAccessReqDetailCtrl', function($scope, $stateParams, $state) {
+  var AccessRequest = Parse.Object.extend("AccessRequest");
+  var query=new Parse.Query(AccessRequest);
+  query.get($stateParams.accessRequestId,{
+    success: function(accessRequest) {
+          $scope.accessRequest=accessRequest;
+          console.log(JSON.stringify(accessRequest));
+        }, 
+        error: function(error) {
+          console.log(error.message);
+        }
+  });
+
+  $scope.approve=function(){
+      $scope.accessRequest.set("status","APPR");
+      $scope.accessRequest.save(null, {
+        success: function(accessRequest) {
+        console.log(JSON.stringify(accessRequest));
+        }
+      });
+      $state.go("tab.adminAccessList");
+      //console.log(JSON.stringify($scope.accessRequest));
+  };
+
+  $scope.reject=function(){
+      $scope.accessRequest.set("status","RJCT");
+      $scope.accessRequest.save(null, {
+        success: function(accessRequest) {
+          console.log(JSON.stringify(accessRequest));
+        }
+    });
+    $state.go("tab.adminAccessList");
+  };
+
+  $scope.cancel=function(){
+    $state.go("tab.adminAccessList");
+  };
+})
+
+.controller('AdminAccessReqListCtrl', function($scope, $state) {
+  $scope.accessRequests=null;
+  $scope.adminAccessRequestListError=null;
+  var AccessRequest = Parse.Object.extend("AccessRequest");
+  var query=new Parse.Query(AccessRequest);
+  //query.include("user");
+  query.equalTo("status", "PEND");
+  query.find({
+    success: function(results) {
+        if(results!=null && results.length>0) {
+            console.log(JSON.stringify(results));
+            $scope.$apply(function(){
+              $scope.accessRequests=results;
+            });
+          } else {
+              //$scope.activityError="No activity found in your region.";
+              $scope.adminAccessRequestListError="No admin access requests to review";
+          }
+        }, 
+        error: function(error) {
+          $scope.adminAccessRequestListError="Unable to get admin access requests";
+        }
+      });
+
 })
 
 .controller('RegisterCtrl', function($scope, $state, $cordovaPush, LogService) {
@@ -122,7 +231,7 @@ angular.module('starter.controllers', ['ngCordova', 'ionic'])
     }
 
   };
-
+  //console.log($scope.user.residency);
   $scope.register=function() {
     // TODO :: Please use angular form validation to validate all the fields
     if($scope.user.phoneNum!=null && $scope.user.phoneNum.length==10) {
@@ -138,7 +247,6 @@ angular.module('starter.controllers', ['ngCordova', 'ionic'])
         user.set("countryCode", $scope.user.countryCode);
         user.set("role", "CTZEN");
         user.set("notifySetting", true);
-
         user.signUp(null, {
           success: function(user) {
             console.log("Signup is success");        
@@ -172,13 +280,59 @@ angular.module('starter.controllers', ['ngCordova', 'ionic'])
     } else {
       $scope.registerErrorMessage="Please enter 10 digit phone number.";
     }
-
   }
 })
 
 .controller('AccountCtrl', function($scope, $state) {
   $scope.user = Parse.User.current();
   $scope.settings={notifications: $scope.user.get("notifySetting")}; 
+  $scope.accessRequestMessage=null;
+  $scope.accessRequest=null;
+  var AccessRequest = Parse.Object.extend("AccessRequest");
+  var query=new Parse.Query(AccessRequest);
+  query.equalTo("user", $scope.user);
+  query.find({
+    success: function(results) {
+        if(results!=null && results.length>0) {
+          console.log(JSON.stringify(results));
+            $scope.$apply(function(){
+              $scope.accessRequest=results[0];
+              if($scope.accessRequest.get("status")=="PEND"){
+                $scope.accessRequestMessage="Your access request request is in review";
+              }
+              else if($scope.accessRequest.get("status")=="APPR"){
+                $scope.user.set("role","ADMN");
+                $scope.user.set("title",$scope.accessRequest.get("title"));
+                $scope.accessRequest.set("status","CMPL");
+                $scope.accessRequest.save(null, {
+                  success: function(accessRequest) {
+                    console.log(JSON.stringify(accessRequest));
+                  }
+                });
+                $scope.user.save(null, {
+                  success: function(user) {
+                    console.log(JSON.stringify(user));
+                  }
+                });
+                console.log(JSON.stringify($scope.user));
+                $scope.accessRequestMessage="Your have admin privileges!"; 
+                console.log($scope.accessRequestMessage);
+              } 
+              else{
+                $scope.accessRequestMessage="Sorry! We cannot give you admin privileges"; 
+              }
+            });
+
+          } else {
+              //$scope.activityError="No activity found in your region.";
+              console.log("no");
+          }
+        }, 
+        error: function(error) {
+          //alert(error.code);
+          
+        }
+      });
 
   $scope.isLogoutAllowed=function() {
     if(ionic.Platform.isAndroid()) {
@@ -192,6 +346,29 @@ angular.module('starter.controllers', ['ngCordova', 'ionic'])
       Parse.User.logOut();
       $scope.user=null;
       $state.go("register");    
+  };
+
+  $scope.isSuperAdmin=function(){
+    var user=Parse.User.current();
+    if(user.get("role")=="SUADM"){
+      return true;
+    }else{
+      return false;
+    }
+  };
+
+  $scope.isCitizen=function(){
+    var user=Parse.User.current();
+    if(user.get("role")=="CTZEN"){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  $scope.getAccessRequestStatus=function() {
+    
+      var user=Parse.User.current();
+
   };
 
   $scope.notifySettingChanged=function(settingName, settingValue) {
