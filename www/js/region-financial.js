c2b48b9734fe1fe01aa117eb9add412b6601f517
Suresh Pragada <spragada@lendingclub.com>
2015-09-24 07:48:26 -0700

    region financial structure

---
+++ www/js/region-financial.js
@@ -0,0 +1,87 @@
+[
+	{
+		"regionUniqueName": "dowlaiswaram", 
+		"year": "2014",
+		"revenue": "11277021.00",
+		"expenses": "17437109.00",
+		"forwardedSurplus": "1684452.00",
+		"surplus": "3917532.00",
+		"regionExpenses": [
+			{
+				"work": "Cleaning supplies",
+				"amount": "1500.00"
+			},
+			{
+				"work": "Cleaning supplies",
+				"amount": "1500.00"
+			}						
+		], 
+		"regionRevenue": [
+			{
+				"source": "Cleaning supplies",
+				"amount": "1500.00"
+			},
+			{
+				"source": "Cleaning supplies",
+				"amount": "1500.00"
+			}						
+		]			
+	},
+	{
+		"regionUniqueName": "dowlaiswaram", 		
+		"year": "2013",
+		"revenue": "15203829.20",
+		"expenses": "17437109.00",
+		"forwardedSurplus": "3917732.00",
+		"reserve": "1684452.00",
+		"regionExpenses": [
+			{
+				"work": "Cleaning supplies",
+				"amount": "1500.00"
+			},
+			{
+				"work": "Cleaning supplies",
+				"amount": "1500.00"
+			}						
+		], 
+		"regionRevenue": [
+			{
+				"source": "Cleaning supplies",
+				"amount": "1500.00"
+			},
+			{
+				"source": "Cleaning supplies",
+				"amount": "1500.00"
+			}						
+		]
+	},
+	{
+		"regionUniqueName": "dowlaiswaram", 		
+		"year": "2012",
+		"revenue": "16281486.00",
+		"expenses": "14771575.00", 												
+		"forwardedSurplus": "2407621.00",
+		"reserve": "3917532.00",
+		"regionExpenses": [
+			{
+				"work": "Cleaning supplies",
+				"amount": "1500.00"
+			},
+			{
+				"work": "Cleaning supplies",
+				"amount": "1500.00"
+			}						
+		], 
+		"regionRevenue": [
+			{
+				"source": "Cleaning supplies",
+				"amount": "1500.00"
+			},
+			{
+				"source": "Cleaning supplies",
+				"amount": "1500.00"
+			}						
+		]
+
+	}
+]
